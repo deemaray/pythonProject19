7caee402c63e44d7957523a8c0049a571ab7033b
Deema Rain <deemarain@Deemas-MacBook-Pro.local>
2024-01-19 12:09:42 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+x = input(" ARE YOU HAPPY ")
+x = x.upper()
+if x == 'YES' or x =='YEAH' or x =='SURE' or x =='OF COURES' or x =='Y' or x =='YAS' or x =='YAZ' or x =='YEP': print('That’s good')
+else:
+    print('That’s bad')
+
+quit()
